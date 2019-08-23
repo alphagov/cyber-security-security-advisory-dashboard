b122680a1b52d93c426d1a47e8179b2dc08bc9c1
Samuel Pritchard <samuel.pritchard@digital.cabinet-office.gov.uk>
2019-08-23 12:33:40 +0100

    cursor checks and count started

--- app.py
+++ app.py
@@ -9,11 +9,31 @@ app = Flask(__name__, static_url_path="/assets")
 @app.route("/")
 def hello_world():
 
-    results = pgraph.query("all", nth=100)
+    cursor = None
+    last = False
+    vulnerability_list = []
+    repository_list = []
 
-    vulnerable_nodes = [
-        node
-        for node in results.organization.repositories.nodes
-        if node.vulnerabilityAlerts.edges
+    while not last:
+
+        if cursor:
+            page = pgraph.query("all", nth=100, after=cursor)
+        else:
+            page = pgraph.query("all", nth=100)
+
+        repository_list.extend(page.organization.repositories.nodes)
+        # page = repositories.organization.repositories.nodes
+        last = not page.organization.repositories.pageInfo.hasNextPage
+        cursor = page.organization.repositories.pageInfo.endCursor
+
+    # results = pgraph.query("all", nth=100)
+    vulnerability_list = [
+        node for node in repository_list if node.vulnerabilityAlerts.edges
     ]
-    return render_template("layout.html", data=vulnerable_nodes)
+
+    repo_count = len(repository_list)
+    vulnerable_count = len(vulnerability_list)
+
+    template_data = {"repo_count": repo_count, "vulnerable_count": vulnerable_count}
+
+    return render_template("layout.html", data=template_data)
