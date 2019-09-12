c58ac4a80fa73302fe8525e18bac8a7d09bd0298
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-09-12 12:21:15 +0100

    Add a history file to the audit output and updated date to the footer.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import json
 import time
@@ -34,19 +35,68 @@ if settings.storage:
     storage.set_options(storage_options)
 
 
+@app.template_filter('iso_date')
+def filter_iso_date(iso_date):
+    """Convert a string to all caps."""
+    parsed = arrow.get(iso_date,"YYYY-MM-DD")
+    return parsed.format("DD/MM/YYYY")
+
+
+def get_history():
+    history_file = "all/data/history.json"
+
+    default = Dict({
+        "current": None,
+        "alltime": {}
+    })
+    history = storage.read_json(history_file, default=default)
+
+    print(str(history), sys.stderr)
+
+    return history
+
+
+def update_history(history):
+    return storage.save_json("all/data/history.json", history)
+
+
+def get_current_audit():
+    try:
+        history = get_history()
+        current = history.current
+    except FileNotFoundError:
+        current = datetime.date.today().isoformat()
+    return current
+
+
 @app.cli.command("audit")
 def cronable_vulnerability_audit():
     today = datetime.date.today().isoformat()
+
+    # set status to inprogress in history
+    history = get_history()
+    history.alltime[today] = "in progress"
+    update_history(history)
+
+    # retrieve data from apis
     org = config.get_value("github_org")
     # todo - set maintenance mode
     repository_status = get_github_repositories_and_classify_by_status(org, today)
     refs_status = get_github_activity_refs_audit(org, today)
     prs_status = get_github_activity_prs_audit(org, today)
     dbot_status = get_dependabot_status(org, today)
+
+    # analyse raw data
     ownership_status = analyse_repo_ownership(today)
     pr_status = analyse_pull_request_status(today)
 
+    # build page template data
     build_route_data(today)
+
+    # update current audit in history
+    history.current = today
+    history.alltime[today] = "complete"
+    update_history(history)
     # todo - set enabled mode
     return True
 
@@ -265,9 +315,10 @@ def route_data_overview_repositories_by_status(today):
             "by_severity": severity_counts,
             "repositories": vulnerable_by_severity,
         },
+        "updated": today
     }
 
-    home_status = storage.save_json(f"{today}/routes/home.json", template_data)
+    home_status = storage.save_json(f"{today}/routes/overview.json", template_data)
     return home_status
 
 
@@ -288,6 +339,7 @@ def get_header():
         "app_name": f"{org_name} Audit"
     }
 
+
 def get_error_data(message):
     return {
         "header": get_header(),
@@ -297,41 +349,110 @@ def get_error_data(message):
         "message": message
     }
 
+
 @app.route("/")
 def route_home():
     try:
-        today = datetime.date.today().isoformat()
+        # today = datetime.date.today().isoformat()
+        today = get_current_audit()
 
         content = {
             "title": "Introduction",
             "org": config.get_value("github_org")
         }
-        return render_template("pages/home.html", header=get_header(), content=content)
+        footer = {
+            "updated": today
+        }
+        return render_template("pages/home.html",
+                               header=get_header(),
+                               content=content,
+                               footer=footer)
     except FileNotFoundError as err:
         return render_template("pages/error.html", **get_error_data("Something went wrong."))
+
 
 @app.route("/overview")
 def route_overview():
     try:
-        today = datetime.date.today().isoformat()
+        # today = datetime.date.today().isoformat()
+        today = get_current_audit()
         content = {
             "title": "Overview"
         }
-        repo_stats = storage.read_json(f"{today}/routes/home.json")
-        return render_template("pages/summary.html", header=get_header(), content=content, data=repo_stats)
+        footer = {
+            "updated": today
+        }
+        repo_stats = storage.read_json(f"{today}/routes/overview.json")
+        return render_template("pages/overview.html",
+                               header=get_header(),
+                               content=content,
+                               footer=footer,
+                               data=repo_stats)
     except FileNotFoundError as err:
         return render_template("pages/error.html", **get_error_data("Something went wrong."))
 
 
-@app.route("/alert-status")
+@app.route("/overview/repository-status")
+def route_overview_repository_status():
+    try:
+        # today = datetime.date.today().isoformat()
+        today = get_current_audit()
+        content = {
+            "title": "Overview - Repository status"
+        }
+        footer = {
+            "updated": today
+        }
+        repo_stats = storage.read_json(f"{today}/routes/overview.json")
+        return render_template("pages/overview_repository_status.html",
+                               header=get_header(),
+                               content=content,
+                               footer=footer,
+                               data=repo_stats)
+    except FileNotFoundError as err:
+        return render_template("pages/error.html", **get_error_data("Something went wrong."))
+
+
+@app.route("/overview/repository-vulnerabilities")
+def route_overview_repository_vulnerabilities():
+    try:
+        # today = datetime.date.today().isoformat()
+        today = get_current_audit()
+        content = {
+            "title": "Overview - Repository vulnerabilities",
+            "org": config.get_value("github_org")
+        }
+        footer = {
+            "updated": today
+        }
+        repo_stats = storage.read_json(f"{today}/routes/overview.json")
+        return render_template("pages/overview_repository_vulnerabilities.html",
+                               header=get_header(),
+                               content=content,
+                               footer=footer,
+                               data=repo_stats)
+    except FileNotFoundError as err:
+        return render_template("pages/error.html", **get_error_data("Something went wrong."))
+
+
+@app.route("/overview/alert-status")
 def route_alert_status():
     try:
-        today = datetime.date.today().isoformat()
+        # today = datetime.date.today().isoformat()
+        today = get_current_audit()
         content = {
             "title": "Overview - Alert status"
         }
+        footer = {
+            "updated": today
+        }
+
         alert_status = storage.read_json(f"{today}/routes/count_alert_status.json")
-        return render_template("pages/alert_status.html", header=get_header(), content=content, data=alert_status)
+        return render_template("pages/overview_alert_status.html",
+                               header=get_header(),
+                               content=content,
+                               footer=footer,
+                               data=alert_status)
     except FileNotFoundError as err:
         return render_template("pages/error.html", **get_error_data("Something went wrong."))
 
@@ -339,9 +460,13 @@ def route_alert_status():
 @app.route("/by-repository")
 def route_owners():
     try:
-        today = datetime.date.today().isoformat()
+        # today = datetime.date.today().isoformat()
+        today = get_current_audit()
         content = {
             "title": "By repository"
+        }
+        footer = {
+            "updated": today
         }
         topics = storage.read_json(f"{today}/data/topics.json")
         repositories = storage.read_json(f"{today}/data/activity_prs.json")
@@ -368,6 +493,7 @@ def route_owners():
             "pages/repo_owners.html",
             header=get_header(),
             content=content,
+            footer=footer,
             other_topics=other_topics,
             team_dict=team_dict,
             repositories=repositories,
