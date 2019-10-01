4e65b92094bd290dfb3d72d4f17ee44ab62b25b3
Alex Kinnane <akinnane@yahoo.co.uk>
2019-10-01 22:38:15 +0100

    Add standard tests and improve test runner

--- test.sh
+++ test.sh
@@ -1,2 +1,11 @@
-black .
-py.test
+py.test \
+    --capture=no \
+    --verbose \
+    --showlocals \
+    -r a \
+    --cov=. \
+    --cov-report html \
+    --cov-fail-under 70 \
+    --flake8 \
+    --black \
+    tests
