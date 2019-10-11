6fd3f0ef53599a20dbe1089c7bb39dcead5849fe
Alex Kinnane <akinnane@yahoo.co.uk>
2019-10-11 13:25:58 +0100

    sec advisories only

--- dependabot_on/cyber_dependabot.py
+++ dependabot_on/cyber_dependabot.py
@@ -11,7 +11,7 @@ ROOT_URL = "https://api.github.com"
 
 def get(path):
     headers = {
-        "Authorization": f'token {os.environ.get("SP_TOKEN", "")}',
+        "Authorization": f'token {os.environ.get("TOKEN", "")}',
         "Accept": "application/vnd.github.dorian-preview+json",
     }
     return requests.get(f"{ROOT_URL}{path}", headers=headers)
@@ -19,7 +19,7 @@ def get(path):
 
 def put(path, data=None):
     headers = {
-        "Authorization": f'token {os.environ.get("SP_TOKEN", "")}',
+        "Authorization": f'token {os.environ.get("TOKEN", "")}',
         "Accept": "application/vnd.github.dorian-preview+json",
     }
     return requests.put(f"{ROOT_URL}{path}", headers=headers)
@@ -35,48 +35,45 @@ def enable_vulnerability_alerts():
 
     for repo in repos:
 
-        if "cyber" in repo.name or "cst" in repo.name or "csw" in repo.name:
-            update_config(repo)
+        #if "cyber" in repo.name or "cst" in repo.name or "csw" in repo.name:
+            # update_config(repo)
 
-            r = get(f"/repos/{repo.owner.login}/{repo.name}/vulnerability-alerts")
-            print(f"repo: {repo.name}, GET: {r.status_code}, {r.text}")
+        r = get(f"/repos/{repo.owner.login}/{repo.name}/vulnerability-alerts")
+        print(f"repo: {repo.name}, GET: {r.status_code}, {r.text}")
 
-            r = put(f"/repos/{repo.owner.login}/{repo.name}/vulnerability-alerts")
-            print(f"repo: {repo.name}, PUT: {r.status_code}, {r.text}")
-
-            r = get(f"/repos/{repo.owner.login}/{repo.name}/vulnerability-alerts")
-            print(f"repo: {repo.name}, GET: {r.status_code}, {r.text}")
+        r = put(f"/repos/{repo.owner.login}/{repo.name}/vulnerability-alerts")
+        print(f"repo: {repo.name}, PUT: {r.status_code}, {r.text}")
 
 
-def update_config(repo):
-    # https://github.com/dependabot/api-docs
-    # https://api.github.com/repositories/1599151
-    if not repo.languages.nodes:
-        return
+# def update_config(repo):
+#     # https://github.com/dependabot/api-docs
+#     # https://api.github.com/repositories/1599151
+#     if not repo.languages.nodes:
+#         return
 
-    for lang in repo.languages.nodes:
-        if not (pm := package_managers.get(lang.name, None)):
-            print(f'Skipping {repo.name}, lang: {lang.name}')
-            continue
+#     for lang in repo.languages.nodes:
+#         if not (pm := package_managers.get(lang.name, None)):
+#             print(f'Skipping {repo.name}, lang: {lang.name}')
+#             continue
 
-        config = {
-            "repo-id": repo.databaseId,
-            "update-schedule": "daily",
-            "directory": "/",
-            "account-id": "596977",
-            "account-type": "org",
-            "package-manager": pm,
-        }
+#         config = {
+#             "repo-id": repo.databaseId,
+#             "update-schedule": "daily",
+#             "directory": "/",
+#             "account-id": "596977",
+#             "account-type": "org",
+#             "package-manager": pm,
+#         }
 
-        root_url = "https://api.dependabot.com"
+#         root_url = "https://api.dependabot.com"
 
-        headers = {
-            "Authorization": f'Personal {os.environ.get("SP_TOKEN", "")}',
-            "Accept": "application/vnd.github.baptiste-preview+json",
-        }
-        r = requests.post(root_url + "/update_configs", headers=headers, json=config)
+#         headers = {
+#             "Authorization": f'Personal {os.environ.get("SP_TOKEN", "")}',
+#             "Accept": "application/vnd.github.baptiste-preview+json",
+#         }
+#         r = requests.post(root_url + "/update_configs", headers=headers, json=config)
 
-        print(f'repo: {repo.name}, config: {lang.name}, r: {r.text}')
+#         print(f'repo: {repo.name}, config: {lang.name}, r: {r.text}')
 
 
 enable_vulnerability_alerts()
