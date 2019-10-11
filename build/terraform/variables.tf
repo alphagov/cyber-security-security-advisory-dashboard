f827448efad39634c20b808cd0ff57ef0ba55538
Alex Kinnane <akinnane@yahoo.co.uk>
2019-10-11 09:29:28 +0100

    Copy bucket config into place

--- build/terraform/variables.tf
+++ build/terraform/variables.tf
@@ -16,23 +16,3 @@ variable "github_org" {
   description = "GitHub organisation short-name"
   type        = "string"
 }
-
-variable "state_bucket" {
-  description = "The state bucket"
-  type        = "string"
-}
-
-variable "stats_key" {
-  description = "The state bucket filename"
-  type        = "string"
-}
-
-variable "state_region" {
-  description = "The region that the state_bucket is in"
-  type        = "string"
-}
-
-variable "state_encrypt" {
-  description = "Is the state_bucket encrypted?"
-  type        = "string"
-}
