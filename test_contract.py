6094df13b0ef498034769b4ace8bf902424357a4
Tristram Oaten <tristram@oaten.name>
2019-11-05 12:00:32 +0000

    add initial contract tests

---
+++ test_contract.py
@@ -0,0 +1,31 @@
+from dependabot_api import get_parsed, get_repos_by_status
+from addict import Dict
+
+import pgraph
+
+
+def test_github_graphql() -> None:
+    """
+    Tests that there are *some* repositories returned by each
+    of the pgraph queries in `query/`
+    """
+    page = Dict(pgraph.query("all", org="alphagov", nth=1, after=None))
+    assert page.organization.repositories.nodes
+
+    page = Dict(pgraph.query("prs", org="alphagov", nth=1, after=None))
+    assert page.organization.repositories.nodes
+
+    page = Dict(pgraph.query("refs", org="alphagov", nth=1, after=None))
+    assert page.organization.repositories.nodes
+
+
+def test_github_dependabot() -> None:
+    """
+    Tests that there are more than 10 active, inactive, by_status and custom, repos.
+    """
+    active = "/repos?account-id=596977&account-type=org&installation-state=active"
+    assert len(get_parsed(active)) > 10
+
+    by_status = Dict(get_repos_by_status("alphagov"))
+    assert len(by_status.active) > 10
+    assert len(by_status.inactive) > 10
