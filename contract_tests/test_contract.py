01a3454f5b7aaf290e14d88879410ff2aa7c3b28
alice-carr <alice.carr@digital.cabinet-office.gov.uk>
2019-12-03 14:46:56 +0000

    remove dependabot api test

--- contract_tests/test_contract.py
+++ contract_tests/test_contract.py
@@ -1,5 +1,4 @@
 import pgraph
-from dependabot_api import get_parsed, get_active_repos
 from addict import Dict
 
 
@@ -12,13 +11,3 @@ def test_github_graphql() -> None:
     for query in ["all", "prs", "refs"]:
         page = Dict(pgraph.query(query, org="alphagov", nth=1, after=None))
         assert page.organization.repositories.nodes
-
-
-def test_github_dependabot() -> None:
-    """
-    Tests that there are more than 10 active repos.
-    """
-    active = "/active_repos?account-id=596977&account-type=org"
-    assert len(get_parsed(active)) > 10
-    by_status = get_active_repos("alphagov")
-    assert len(by_status) > 10
